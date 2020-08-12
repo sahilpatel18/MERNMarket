68a9101e8ddc2e5770a6a32f4258fdf3da5f4169
sahilpatel18 <sahil.41801@gmail.com>
2020-08-12 18:10:17 -0400

    mongoose connection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,11 +8,10 @@ const URI = process.env.MONGO_URI;
 const shoproutes = require("./routes/index");
 const cookieParser = require("cookie-parser");
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use("/api", shoproutes);
 
- 
 mongoose.connect(
   URI,
   { useNewUrlParser: true, useUnifiedTopology: true },
@@ -25,53 +24,6 @@ mongoose.connect(
   }
 );
 
-
-// const Order = require("./models/order.model");
-
-// const userInput = {
-//   username: "tomjerry12",
-//   password: "abc",
-// };
-
-// const us = new User(userInput);
-// us.save((err, doc) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("user is now in the database");
-//   }
-// });
-
-// const order1 = {
-//   products: [
-//     {
-//       _id: "5f31cf4384cfa5581b82da53",
-//       name: "Journal",
-//       price: 12.99,
-//       quantity: 1,
-//     },
-//     {
-//       _id: "5f31cf4384cfa5581b82da54",
-//       name: "Mat",
-//       price: 20.99,
-//       quantity: 1,
-//     },
-//   ],
-//   user: {
-//     _id: "5f32b363f8fac45f55a77dac",
-//     username: "tomjerry12",
-//     password: "abc",
-//   },
-// };
-
-// const data = new Order(order1);
-// data.save((err, doc) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("good");
-//   }
-// });
 app.listen(PORT, () => {
   console.log(`your server is running on port ${PORT}`);
 });
