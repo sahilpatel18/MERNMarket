32ce6c5f2a249bd8b7be7b76f526c213c43be70f
sahilpatel18 <sahil.41801@gmail.com>
2020-08-12 18:12:28 -0400

    added imageURL to schema and individual product review schema to ProductSchema

--- models/product.model.js
+++ models/product.model.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
+const Review = require("./review.model").schema;
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
   name: String,
   price: Number,
-  quantity: Number
+  quantity: Number,
+  imgURL: String,
+  reviews: [Review],
 });
 
 const Product = mongoose.model("Product", ProductSchema);
 
 module.exports = Product;
-
-
