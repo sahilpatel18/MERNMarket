c90f1f186f18322bca4559e9c8f76a99e980b2cd
sahilpatel18 <sahil.41801@gmail.com>
2020-08-13 16:45:08 -0400

    mapping over each product and passing name and price as props

---
+++ client/src/components/Home.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import ProductCard from "./ProductCard";
+
+const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(`/api/products`);
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const data = await response.json();
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="row justify-content-center">
+        {products.map((product) => {
+          return <ProductCard name={product.name} price={product.price} />;
+        })}
+      </div>
+  );
+};
+
+export default Home;
