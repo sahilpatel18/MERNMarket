dbec7388f131f6cad6d3cd94852071b8d11240a7
sahilpatel18 <sahil.41801@gmail.com>
2020-08-13 15:24:32 -0400

    created homepag,login, and register routes using react-router-dom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Navbar from "./Navbar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from './Home'
+import Login from "./Login";
+import Register from "./Register";
 
 const App = () => {
-  return <div>App</div>;
+
+  return (
+    <BrowserRouter>
+      <div className='app'>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
