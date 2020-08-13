408896f0a6d71202daf7d6f50a13472ac9cd0f41
sahilpatel18 <sahil.41801@gmail.com>
2020-08-13 15:25:01 -0400

    bootstrap navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
+      <Link className='navbar-brand' to='/'>
+        MERN
+      </Link>
+      <button
+        className='navbar-toggler'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navbarNavAltMarkup'
+        aria-controls='navbarNavAltMarkup'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <span className='navbar-toggler-icon'></span>
+      </button>
+      <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
+        <div className='navbar-nav'>
+          <Link className='nav-item nav-link active' to='/'>
+            Products<span className='sr-only'>(current)</span>
+          </Link>
+          <Link className='nav-item nav-link' to='/login'>
+            Login
+          </Link>
+          <Link className='nav-item nav-link' to='/register'>
+            Register
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
