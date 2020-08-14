afc91724a41c123fedf02ce34af55cee5228037d
sahilpatel18 <sahil.41801@gmail.com>
2020-08-14 18:23:11 -0400

    passed in the imageURL

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ProductCard = ({ product: { _id: id, name, price } }) => {
+const ProductCard = ({ product: { _id: id, name, price, imageURL } }) => {
   return (
     <div className='py-5'>
       <div className='card text-center' style={{ width: "18rem" }}>
         <div className='card-block'>
-          <img className='card-img-top' src='...' alt='Card image cap' />
+          <img className='card-img-top' src={imageURL} alt='Card image cap' />
           <div className='card-body'>
             <h5 className='card-title'>{name}</h5>
             <p className='card-text'>Price: {price}</p>
