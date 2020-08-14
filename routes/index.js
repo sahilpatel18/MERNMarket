736574699bc041be404171b39d3c992bfa10b823
sahilpatel18 <sahil.41801@gmail.com>
2020-08-14 18:24:57 -0400

    shopping cart design layout

--- routes/index.js
+++ routes/index.js
@@ -55,7 +55,7 @@ router.post("/register", async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
-    res.send(user);
+    res.send('User Saved To DB');
   }
 });
 
