ff077de83946d05290ac2999a39cb71c8ee68034
sahilpatel18 <sahil.41801@gmail.com>
2020-08-14 17:08:25 -0400

    create new route for the product details page and cart page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from './Home'
 import Login from "./Login";
 import Register from "./Register";
+import productDetails from "./productDetails";
+import Cart from './Cart';
 
 const App = () => {
 
@@ -15,6 +17,8 @@ const App = () => {
           <Route path='/products' exact component={Home} />
           <Route path='/login' component={Login} />
           <Route path='/register' component={Register} />
+          <Route path={`/products/:id`} component={productDetails} />
+          <Route path='/cart' component={Cart} />
         </Switch>
       </div>
     </BrowserRouter>
