dcc300fa250d2b4df4ce8c87197192aa3a7ba74d
sahilpatel18 <sahil.41801@gmail.com>
2020-08-18 11:55:43 -0400

    cart reducer that handles product being added to cart and updates the total price

--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -1,10 +1,19 @@
+import actions from "../actions";
 
 const initialState = {
-    addedItems:[],
-    total:0
-}
+  addedItems: [],
+  total: 0,
+};
 
+const cartReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "ADD_TO_CART":
+      return { ...state, addedItems: [...state.addedItems, payload] };
+    case "UPDATE_TOTAL":
+      return { ...state, total: state.total + payload };
+    default:
+      return state;
+  }
+};
 
- const cartReducer = (initialState, action) => {
-
- }
+export default cartReducer;
