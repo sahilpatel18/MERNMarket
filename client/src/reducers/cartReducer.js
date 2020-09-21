7128e2ed855423c2853e2fcdf965bc5436238916
sahilpatel18 <sahil.41801@gmail.com>
2020-09-21 14:54:30 -0400

    cart reducer to remove and add items to cart

--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -1,4 +1,9 @@
-import actions from "../actions";
+import {
+  TERMINATE_CART,
+  REMOVE_FROM_CART,
+  ADD_TO_CART,
+  UPDATE_TOTAL,
+} from "../actions/types";
 
 const initialState = {
   addedItems: [],
@@ -7,18 +12,38 @@ const initialState = {
 
 const cartReducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    case "ADD_TO_CART":
-      return { ...state, addedItems: [...state.addedItems, payload] };
-    case "UPDATE_TOTAL":
-      return { ...state, total: state.total + payload };
-    case "REMOVE_FROM_CART":
+    case REMOVE_FROM_CART:
       return {
-        addedItems:[...state.addedItems.filter(item => item !== payload)]
+        addedItems: [...state.addedItems.filter((item) => item !== payload)],
+        total: state.total - payload.price * payload.quantity,
+      };
+    case TERMINATE_CART:
+      return {
+        ...state,
+        addedItems: [],
+        total: 0,
+      };
+    case ADD_TO_CART:
+      let doesExist = state.addedItems.find((item) => item._id === payload._id);
+      if (doesExist !== undefined) {
+        doesExist.quantity = +doesExist.quantity + +payload.quantity;
+        state.addedItems.splice(
+          state.addedItems.indexOf(doesExist),
+          1,
+          doesExist
+        );
+        return { ...state, addedItems: [...state.addedItems] };
+      } else {
+        return {
+          ...state,
+          addedItems: [...state.addedItems, payload],
+        };
       }
+    case UPDATE_TOTAL:
+      return { ...state, total: state.total + payload };
     default:
       return state;
   }
 };
 
 export default cartReducer;
- 
