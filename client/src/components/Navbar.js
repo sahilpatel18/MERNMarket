4f3d03a22f998f64a5f57a806650430824bb85e1
sahilpatel18 <sahil.41801@gmail.com>
2020-09-21 15:00:30 -0400

    allow logged in users to see order history

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { logoutUser } from "../actions/index";
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Navbar = () => {
+  let auth = useSelector((state) => state.auth.isAuthenticated);
+  let dispatch = useDispatch();
+  const history = useHistory();
+
+  const onLogoutClick = (e) => {
+    e.preventDefault();
+    dispatch(logoutUser());
+    toast.error("Logged Out", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+    history.push("/login");
+  };
+
   return (
     <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
-      <Link className='navbar-brand' to='/products'>
+      <Link className='navbar-brand' to='/'>
         MERN
       </Link>
       <button
@@ -20,18 +43,41 @@ const Navbar = () => {
       </button>
       <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
         <div className='navbar-nav'>
-          <Link className='nav-item nav-link active' to='/products'>
-            Products<span className='sr-only'>(current)</span>
-          </Link>
-          <Link className='nav-item nav-link' to='/login'>
-            Login
-          </Link>
-          <Link className='nav-item nav-link' to='/register'>
-            Register
-          </Link>
-          <Link className="nav-item nav-link" to='/cart'>
-          Cart
-          </Link>
+          {auth ? (
+            <>
+              <Link className='nav-item nav-link active' to='/'>
+                Products<span className='sr-only'>(current)</span>
+              </Link>
+              <Link
+                onClick={onLogoutClick}
+                className='nav-item nav-link'
+                to='/register'
+              >
+                Logout
+              </Link>
+              <Link className='nav-item nav-link' to='/cart'>
+                Cart
+              </Link>
+              <Link className='nav-item nav-link' to='/userorder'>
+                Orders
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link className='nav-item nav-link active' to='/'>
+                Products<span className='sr-only'>(current)</span>
+              </Link>
+              <Link className='nav-item nav-link' to='/login'>
+                Login
+              </Link>
+              <Link className='nav-item nav-link' to='/register'>
+                Register
+              </Link>
+              <Link className='nav-item nav-link' to='/cart'>
+                Cart
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
