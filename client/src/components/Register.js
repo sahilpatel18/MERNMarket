b4210797528d19da371518d095e3f74a2999231b
sahilpatel18 <sahil.41801@gmail.com>
2020-09-21 14:59:51 -0400

    dispatches action to send user input to the backend and store user in MongoDB

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,36 +1,69 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { registerUser } from "../actions/index";
 
 const Register = () => {
-  const [user, setUser] = useState("");
+  let history = useHistory();
+  let dispatch = useDispatch();
+  let auth = useSelector((state) => state.auth.isAuthenticated);
 
-  const postRegisteredUser = async () => {
-    const response = await axios.post("/api/register", user);
-    if (!response.ok) {
-      throw new Error(response.statusText);
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+    errors: {},
+  });
+
+  useEffect(() => {
+    if (auth) {
+      history.push("/products");
     }
-    console.log(response);
+  });
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      username: user.username,
+      password: user.password,
+    };
+    history.push("/login");
+    dispatch(registerUser(userData));
+  };
+
+  const onChangeHandler = (e) => {
+    setUser({
+      ...user,
+      [e.target.id]: e.target.value,
+    });
   };
 
   return (
     <div className='container py-5'>
       <h1>Register</h1>
-      <form onSubmit={postRegisteredUser}>
+  
+      <form onSubmit={onSubmitHandler}>
         <div className='form-group'>
           <label for='username'>Username</label>
           <input
-            onChange={(e) => setUser({ ...user, username: e.target.value })}
+            onChange={onChangeHandler}
             type='text'
+            value={user.username}
+            id='username'
             name='username'
+            error={user.errors.username}
             className='form-control'
           />
         </div>
         <div className='form-group'>
           <label for='password'>Password</label>
           <input
-            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            onChange={onChangeHandler}
             type='password'
+            value={user.password}
+            error={user.errors.password}
             name='password'
+            id='password'
             className='form-control'
           />
         </div>
@@ -38,8 +71,11 @@ const Register = () => {
           Register
         </button>
       </form>
+      <p className='grey-text text-darken-1'>
+        Already have an account? <Link to='/login'>Log in</Link>
+      </p>
     </div>
   );
 };
 
-export default Register;
+export default Register
