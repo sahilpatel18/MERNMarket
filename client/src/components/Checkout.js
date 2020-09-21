839a5b5daf6387f90273e459e576a6c27a50c9f8
sahilpatel18 <sahil.41801@gmail.com>
2020-09-21 14:53:38 -0400

    getting user data on checkout page from redux store

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,39 +1,65 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { postUserOrder } from "../actions";
 
 const Checkout = () => {
+  let dispatch = useDispatch();
+  let history = useHistory();
+  let total = useSelector((state) => state.cart.total);
+  let items = useSelector((state) => state.cart.addedItems);
+  let accountUser = useSelector((state) => state.auth.user);
+  let totalAmount = (Math.round(total * 100) / 100).toFixed(2);
+
+  let result = (items || []).map((product) => {
+    return {
+      _id: product._id,
+      name: product.name,
+      price: product.price,
+      quantity: product.quantity,
+    };
+  });
+
+  const myData = {
+    products: result,
+    userId: accountUser.id,
+    username: accountUser.name,
+    total: totalAmount,
+  };
+  const handleClick = () => {
+    dispatch(postUserOrder(myData));
+    history.push("/order");
+  };
+
   return (
     <div className='container'>
-      <div className='card mb-4'>
+      <div className='card mb-4 mt-3'>
         <div className='card-body'>
-          <h5 className='mb-3'>The total amount of</h5>
+          <h5 className='mb-3'>Checkout</h5>
 
           <ul className='list-group list-group-flush'>
             <li className='list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0'>
               Temporary amount
-              <span>$53.98</span>
+              <span>${totalAmount}</span>
             </li>
             <li className='list-group-item d-flex justify-content-between align-items-center px-0'>
               Shipping
-              <span>Gratis</span>
+              <span>Free</span>
             </li>
             <li className='list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3'>
               <div>
-                <strong>The total amount of</strong>
-                <strong>
-                  <p className='mb-0'>(including VAT)</p>
-                </strong>
+                <strong>Total To Pay</strong>
               </div>
               <span>
-                <strong>$53.98</strong>
+                <strong>${totalAmount}</strong>
               </span>
             </li>
           </ul>
-
           <button
-            type='button'
-            className='btn btn-primary btn-block waves-effect waves-light'
+            onClick={handleClick}
+            className='btn btn-primary btn-block mt-3 waves-effect waves-light'
           >
-            go to checkout
+            <span>Pay</span>
           </button>
         </div>
       </div>
