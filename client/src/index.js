2dfe78cfffca970dd830921cecccb9c1cc6870a9
sahilpatel18 <sahil.41801@gmail.com>
2020-09-21 15:13:44 -0400

    redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import {composeWithDevTools} from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
-import reducer from './reducers/cartReducer'
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import reducer from "./reducers";
 
-
- const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
-
+export const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 ReactDOM.render(
   <Provider store={store}>
     <App />
